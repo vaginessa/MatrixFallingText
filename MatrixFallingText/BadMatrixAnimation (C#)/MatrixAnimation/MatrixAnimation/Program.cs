0e81631536f9e99b307ed0deb21049ade3310ac6
GREBTSEW <danwe980@student.liu.se>
2016-09-22 22:04:29 +0200

    Added files

---
+++ MatrixFallingText/BadMatrixAnimation (C#)/MatrixAnimation/MatrixAnimation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixAnimation
+{
+    class Program
+    {
+        static Random _random = new Random();
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+           
+            Console.WriteLine("Hello, world!");
+
+            while (true) {
+                int times = _random.Next(20, 30);
+                int space = _random.Next(0, 5);
+                string s = "";
+                string temp = " ";
+                for(int j = 0; j < times; j++) { 
+                for(int i = 0; i < space; i++)
+                {
+                    temp += temp;
+                }
+                    s += GetLetter() + temp;
+                    temp = " ";
+                }
+
+           
+            Console.WriteLine(s);
+                System.Threading.Thread.Sleep(50);
+            }
+
+           
+        }
+
+        public static char GetLetter()
+        {
+            // This method returns a random lowercase letter.
+            // ... Between 'a' and 'z' inclusize.
+            int num = _random.Next(0, 26); // Zero to 25
+            char let = (char)('a' + num);
+
+            return let;
+        }
+    }
+}
---
+++ MatrixFallingText/BadMatrixCode.bat
@@ -0,0 +1,14 @@
+@echo off
+title AHHHHHH
+color 02
+echo This Matrix Affect was created by Superman
+echo This program will shut down your computer!
+echo The Matrix Effect begins in
+TIMEOUT 5
+
+
+:superman
+PING 1.1.1.1 -n 1 -w 0.2 >NUL
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto superman
+
